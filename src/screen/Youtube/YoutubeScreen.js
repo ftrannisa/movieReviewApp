12996e03c2fd54fb5cabd1b2862011c1057752f9
Fitri Annisa Ahlul Jannah <ftrannisaa@gmail.com>
2020-10-09 12:06:14 +0700

    fix styling youtube

--- src/screen/Youtube/YoutubeScreen.js
+++ src/screen/Youtube/YoutubeScreen.js
@@ -1,16 +1,19 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import YouTube from 'react-native-youtube';
+import {header} from '../../assets';
 
 const YTubeApi = 'AIzaSyDeQojP5P2dQkVzDwI_22E7GgMyjJXkCSs'
 
 
 const YouTubeScreen = () => {
     return (
-        <View>
-            <Text>test video youtube</Text>
-            <YouTube
+        <View style={{backgroundColor: 'black', flex: 1, padding: 10}}>
+            <Image source={header} style={styles.header} />
+            <View style={{flex: 1, justifyContent: 'center'}}>
+                <Text style={styles.textSection}>Trailer</Text>
+                <YouTube
                 apiKey={YTubeApi}
                 videoId="1d0Zf9sXlHk" // The YouTube video ID
                 play // control playback of video with true/false
@@ -20,10 +23,28 @@ const YouTubeScreen = () => {
                 // onChangeState={e => this.setState({ status: e.state })}
                 // onChangeQuality={e => this.setState({ quality: e.quality })}
                 // onError={e => this.setState({ error: e.error })}
-                style={{ alignSelf: 'stretch', height: 300 }}
-            />
+                style={{alignSelf: 'stretch', height: 300 }}
+             />
+            </View>
         </View>
     )
 }
 
 export default YouTubeScreen
+
+const styles = StyleSheet.create({
+    header: {
+        height: 50,
+        width: 120,
+        alignSelf: 'center',
+      },
+    textSection: {
+        color: 'white',
+        fontSize: 18,
+        letterSpacing: 2,
+        marginBottom: 5,
+        fontFamily: 'Roboto',
+        textAlign: 'center'
+      },
+
+})
